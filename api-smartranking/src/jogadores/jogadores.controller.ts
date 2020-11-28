380abdb766617247439b9ec43888b96b328a9141
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-11-28 07:52:34 -0300

    Aula 12

---
+++ api-smartranking/src/jogadores/jogadores.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from "./dtos/criar-jogador.dto";
+
+@Controller('api/v1/jogadores')
+export class JogadoresController {
+
+    @Post()
+    async criarAtualizarJogador(
+        @Body() criarJogadorDto: CriarJogadorDto
+    ) {
+        const { nome } = criarJogadorDto;
+        return {
+            "Nome": nome
+        }
+    }
+}
