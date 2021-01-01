63f9dec7676e545c4b8f2e517e5514bdcf9e7d58
Brian Leonardo de Morais <morais.brian360@gmail.com>
2021-01-01 16:33:35 -0300

    Inicialização de variáveis de ambiente no projeto

--- Monolith/api-smartranking/src/main.ts
+++ Monolith/api-smartranking/src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
 import * as momentTimezone from "moment-timezone";
+import dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionFilter);
 
