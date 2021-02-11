bc014b782712f570be067ae7993e08a8370d2e7f
Brian Leonardo de Morais <morais.brian360@gmail.com>
2021-02-11 11:01:26 -0300

    Aula 40

--- Microservices/api-gateway/src/app.controller.ts
+++ Microservices/api-gateway/src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Logger, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  private logger = new Logger(AppController.name);
+
+  private clientAdminBackend: ClientProxy;
+
+  constructor() { 
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://USUARIO:SENHA@IP:PORTA/VIRTUAL_HOST'],
+        queue: 'admin-backend'
+      }
+    });
+  }
+
+  @Post('categorias')
+  @UsePipes(ValidationPipe)
+  async criarCategoria(@Body() criarCategoriaDto: CriarCategoriaDto) {
+    return await this.clientAdminBackend.emit('criar-categoria', criarCategoriaDto);
   }
 }
